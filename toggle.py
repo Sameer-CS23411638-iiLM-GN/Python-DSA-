dbc75c79aa157214801d7b21b3b9c26cbe52173d
Sameer <sameerlntmhipower@gmail.com>
2026-03-26 12:53:26 +0530

    Add files via upload

---
+++ toggle.py
@@ -0,0 +1,15 @@
+# ans = start ^ global
+# count = 0
+# for i in range(0,32):
+#     if ans & (1 << i):
+#         count += 1
+# print(count)
+
+# ans = start ^ g
+# count = 0
+
+# for i in range(0, 32):
+#     if ans & (1 << i):
+#         count += 1
+
+# print(count)
