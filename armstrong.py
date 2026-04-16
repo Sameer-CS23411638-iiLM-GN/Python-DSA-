d06cf5d1a44c808ec17d531fca94cfe29d115aa5
Sameer <sameerlntmhipower@gmail.com>
2026-04-16 09:38:57 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,48 @@
+# n = int(input('enter any number: '))
+# num = n
+# total = 0
+# nod = len(str(num))
+# while num>0:
+#     ld = num%10
+#     total = total + ld**nod
+#     num = num//10
+# if total == n:
+#     print("armstrong")
+# else:    print("not armstrong")
+
+n = int(input('enter any number: '))
+num = n 
+total = 0
+nod = len(str(num))
+while num>0:
+    ld = num%10
+    total = total = + ld**nod
+    num = num//10
+if total == n:
+    print("armstrong")
+else:
+    print("not armstrong")
+
+# n  = int(input("enter the number: "))
+# num = n
+# total = 0
+# nod = len(str(num))
+# while num>0:
+#     ld = num%10
+#     total = total + ld**nod
+#     num = num//10
+#     if total == n:
+#         print("armstrong")
+
+# n = int(input("enter the number: "))
+# num = n
+# total = 0
+# nod = len(str(num))
+# while num>0:
+#     ld = num%10
+#     total = total + ld**nod
+#     num = num//10
+#     if total == n:
+#         print("armstrong ")
+#     else:
+#         print("not armstrong")
