5e06581bdae5a190af88852c280026a26f5b7e87
Sameer <sameerlntmhipower@gmail.com>
2026-04-25 11:16:34 +0530

    Add files via upload

--- dlte_occurence.py
+++ dlte_occurence.py
@@ -1,13 +1,13 @@
-#brute force approach
-while temp is not None:
-  if temp.val == key:
-    if prev is not None:
-      prev.next = temp.next
-    if temp.next is not None:
-      temp.next.prev = prev
-    if temp == new_head:
-        new_head = new_head.next
-    prev = temp
-    temp = temp.next
-return new_head
+# #brute force approach
+# while temp is not None:
+#   if temp.val == key:
+#     if prev is not None:
+#       prev.next = temp.next
+#     if temp.next is not None:
+#       temp.next.prev = prev
+#     if temp == new_head:
+#         new_head = new_head.next
+#     prev = temp
+#     temp = temp.next
+# return new_head
   
