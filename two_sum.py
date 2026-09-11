c5674ab798c2d08f1300d8c5ee03b65199708e0e
Sameer <sameerlntmhipower@gmail.com>
2026-09-11 23:52:32 +0530

    Add files via upload

--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,36 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        num_map = {}
+nums = [5,9,1,2,4,15,6,3]
+target = 10
 
-        for i, num in enumerate(nums):
-            res = target - num
-            if res in num_map:
-                return [num_map[res], i]
-            num_map[num] = i
+#brute force approach
+# n = len(nums)
+# for i in range(0,n-1):
+#     for j in range(i+1,n):
+#         if nums[i]+nums[j] == target:
+#             print(i,j)
+
+#optimal approach
+n = len(nums)
+hash_map = {}
+
+for i in range(n):
+    remaining = target - nums[i]
+    
+    if remaining in hash_map:
+        print([hash_map[remaining], i])
+        break   # stop after finding pair
+    
+    hash_map[nums[i]] = i
+
+# n - len(nums)
+# hash_map = {}
+# for i in range(0,n):
+#     remaining = target - nums[i]
+
+#     if remaining in hash_map:
+#         print([hash_map[remaining],i])
+#         break
+
+#     hash_map[nums[i]] - i
+
+
+    
